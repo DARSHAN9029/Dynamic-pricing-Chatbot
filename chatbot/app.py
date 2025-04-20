06fc49c257f004218182af6171252e6bd9cbd366
darshan9029@gmail.com <darshan9029@gmail.com>
2025-04-20 13:13:22 +0530

    updated

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-from chatbot.query_bot import get_bot_response
-
-app = Flask(__name__)
 from query_bot import get_bot_response
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-
 CORS(app, origins=["http://localhost:5173"])
 
 @app.route("/")
@@ -21,4 +16,6 @@ def chat():
     return jsonify({"reply": bot_reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
